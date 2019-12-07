1c9061765c914f0e0ccf76e06b30ace5c3128feb
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2019-12-07 12:59:23 +0530

    Added the easy working solution for EXUNA

--- EXUNA/program.py
+++ EXUNA/program.py
@@ -1,9 +1,9 @@
-t=int(input())
-for i in range(0,t):
-    n=int(input())
+# EXUNA
+# Weird Modulo Problem
+no=int(input())
+
+for _ in range(no):
+    input()
     l=list(map(int,input().split()))
     l.sort()
-    s=l[0]
-    for j in range(1,n):
-        s=s%l[j]
-    print(s)
+    print(l[0])
